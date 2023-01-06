197b66ece2a245bddb14622961cc67b6abc8e74b
FIRST_NAME LAST_NAME <60664170+henry970@users.noreply.github.com>
2023-01-06 10:47:31 +0100

    "update selenium file"

---
+++ my_test/suite_2_test.py
@@ -0,0 +1,10 @@
+
+
+class TestCheckout:
+
+    def test_check_as_guest(self):
+        print("Checkout quest")
+
+    def test_checkout_existing_user(self):
+        print("checkout existing user.")
+
---
+++ my_test/test_suite_1.py
@@ -0,0 +1,13 @@
+# import pytest
+#
+# @pytest.mark.smoke
+def test_login_page_valid_user():
+    print("login with valid user")
+
+# @pytest.mark.smoke
+# @pytest.mark.regression
+def test_login_wrong_password():
+    print("login with wrong password")
+    # assert 1==2, 'failed'
+
+
